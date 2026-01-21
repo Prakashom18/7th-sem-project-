9b23ff9a3d6403f22138de6bc6df8074940c08cc
Prakashom18 <shahomprakash097@gmail.com>
2026-01-21 21:46:02 +0545

    Authentication

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const User = require('../models/userModel');
-
+const jwt = require('jsonwebtoken');
 const router = express.Router();
+
+const generateToken = (user)=>{
+    return jwt.sign({
+        username : user.username,
+        id : user._id
+    },'secret123')
+}
 
 router.get('/',(req,res)=>{
     res.render('login');
@@ -15,7 +22,7 @@ router.get('/register',(req,res)=>{
 router.post('/register',async (req,res)=>{
     try{
         let {username,password} = req.body;
-        let user = await User.findOne({email : email});
+        let user = await User.findOne({username : username});
         if(user) return res.status(401).send("You have already an account");
         bcrypt.genSalt(10,(err,salt)=>{
             bcrypt.hash(password,salt, async (err,hash)=>{
@@ -23,9 +30,11 @@ router.post('/register',async (req,res)=>{
                 username,
                 password :hash
                 })
+                console.log(user);
                 let token = generateToken(user);
                 res.cookie("token",token);
-                res.send("user created successful");
+                res.redirect('/');
+                // res.send("user created successful");
             })
 
         })
@@ -37,5 +46,33 @@ router.post('/register',async (req,res)=>{
     //res.redirect('/')
 })
 
+// router.post('/login', async (req, res) => {
+//     const user = await User.findOne({ username: req.body.username });
+//     if (user && await bcrypt.compare(req.body.password, user.password)) {
+//         req.session.user = user;
+//         // res.redirect('/courses');
+//         res.send('logged in')
+//     } else {
+//         res.send("Invalid login");
+//     }
+// });
+
+router.post('/login',async (req,res)=>{
+    let {username,password} = req.body;
+    let user = await User.findOne({username});
+    if(!user) {
+        return res.status(201).send('Email  incorrect');
+    }
+    bcrypt.compare(password,user.password,(err,result)=>{
+        if(result){
+            let token = generateToken(user);
+            res.cookie('token',token);
+            res.send('Loogedin');
+        }
+        else{
+            res.send('Email or password incorrect');
+        }
+    })
+})
 
 module.exports = router;
